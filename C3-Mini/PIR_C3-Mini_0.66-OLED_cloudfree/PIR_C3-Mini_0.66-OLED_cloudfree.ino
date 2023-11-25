471374b740eb33ef72c9a8265c04e9001b58d72e
TIMOTHYMWALA <mwalatimo@gmail.com>
2023-11-25 20:51:00 +0300

    Update PIR_C3-Mini_0.66-OLED_cloudfree.ino

--- C3-Mini/PIR_C3-Mini_0.66-OLED_cloudfree/PIR_C3-Mini_0.66-OLED_cloudfree.ino
+++ C3-Mini/PIR_C3-Mini_0.66-OLED_cloudfree/PIR_C3-Mini_0.66-OLED_cloudfree.ino
@@ -2,41 +2,51 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define OLED_RESET 22 //Correspond to GPIO0 on ESP8266
+#define OLED_RESET 0 // Corresponds to GPIO0 on ESP32C3
 Adafruit_SSD1306 display(OLED_RESET);
 
-const int PIR = 2;
-int PIRState = LOW;
+const int PIR = 7;
+int PIRState = LOW; // Set the initial state of PIR to LOW
  
 void setup() 
 {
   Serial.begin(9600);
-    Wire.begin(8,10);
+  Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE);
- delay(2000);
-  pinMode(PIR, INPUT);  
+  delay(2000);
+ 
+  pinMode(PIR, INPUT);
 }
  
 void loop() 
 {
+  // intialize by Clearing the display.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  
   PIRState = digitalRead(PIR);
  
   if (PIRState == HIGH) 
   {
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(35, 15);
-    display.write("INTRUDER!\n");
-    display.display(); 
+    display.setCursor(35, 8);
+    display.print("P.detector");
+    display.setCursor(35, 20);
+    display.println("INTRUDER!");
+    display.display();
   } 
   else 
   {
     display.clearDisplay();
     display.setTextSize(1);
-    display.setCursor(40,15);
-    display.write("No motion\n");
+    display.setCursor(35, 8);
+    display.print("P.detector");
+    display.setCursor(35, 20);
+    display.println("No motion");
     display.display();
   }
+  
   delay(1000);
 }
